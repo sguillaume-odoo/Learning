4094f80af743066d212d001625d544b492a1bbf9
sguillaume-odoo <odoo@ampd1.odoo.com>
2021-02-19 21:25:54 +0000

    Added Open Academy Module - wizard.py

--- openacademy/.ipynb_checkpoints/wizard-checkpoint.py
+++ openacademy/.ipynb_checkpoints/wizard-checkpoint.py
@@ -12,11 +12,11 @@ class Wizard(models.TransientModel):
     session_ids = fields.Many2many('openacademy.session',
         string="Sessions", required=True, default=_default_sessions)
     
-    #def _default_session(self):
-    #    return self.env['openacademy.session'].browse(self._context.get('active_id'))
+    def _default_session(self):
+        return self.env['openacademy.session'].browse(self._context.get('active_id'))
 
-    #session_id = fields.Many2one('openacademy.session',
-    #     string="Session", required=True, default=_default_session)
+    session_id = fields.Many2one('openacademy.session',
+         string="Session", required=True, default=_default_session)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
     
     def subscribe(self):
