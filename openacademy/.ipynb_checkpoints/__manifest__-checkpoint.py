de5bcef856c69a82c3cfa2673a39944724978ad2
sguillaume-odoo <odoo@ampd1.odoo.com>
2021-02-19 21:20:01 +0000

    Added Open Academy Module - partner.py

--- openacademy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ openacademy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -21,7 +21,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'openacademy'],
+    'depends': ['base', 'board'],
 
     # always loaded
     'data': [
